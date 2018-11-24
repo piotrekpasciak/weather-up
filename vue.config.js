afc6ad4781809b68be0b400d6069303345e9bfd3
igorbezsmertnyi <bezsmertnyi.igor@gmail.com>
2018-11-24 14:17:39 +0100

    config circle ci

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+const baseUrl = process.env.BASE_URL ? process.env.BASE_URL : '/'
+
+module.exports = {
+  baseUrl: baseUrl,
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].template = 'public/index.html'
+        return args
+      })
+  }
+}
